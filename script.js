4f70e1976c1e6da1437e9304c1c8488eaaf6f0cb
Dannydoesdev <daniel.t.mcgee@gmail.com>
2022-03-19 17:15:48 +1100

    Got input adding and removing from divs on screen

--- script.js
+++ script.js
@@ -3,7 +3,95 @@
 //check if the inputs match any of the wordle of today
 // 
 
-console.log(words.length)
+// console.log(words.length)
+
+let inputArr = []
+
+const input = document.getElementById('input')
+
+const submitButton = document.getElementById('submit')
+
+let userInput = input.value
+
+
+
+// input.addEventListener('input', checkInput)
+
+input.addEventListener('keyup', checkInput)
+
+// input.addEventListener('keyup', function (e) {
+//     console.log(e)
+// })
+
+
+function checkInput(e) {
+    // userInput.innerText = userInput.toLowerCase()
+    console.log(e)
+    // let currentLetter = e.data
+    let keypress = e.code
+
+    //give currentLetter something outside loop
+    let currentLetter
+
+    if (keypress.includes('Key')) {
+        let currentLetter = e.key
+        currentLetter.toLowerCase()
+        console.log('thisletter' + currentLetter)
+        inputArr.push(currentLetter)
+    }
+
+    //if backspace is used remove a letter
+    else if (keypress.includes('Backspace'))
+    {
+        inputArr.pop()
+        }
+    // if (currentLetter)
+
+// console.log(inputArr)
+    
+    // console.log(e.target.value)
+    // inputArr.push(currentLetter)
+    // console.log(inputArr)
+
+    // // updateDivs(currentLetter);
+    updateDivs(inputArr);
+}
+
+
+
+
+function updateDivs(letter) {
+    let letterRow = document.querySelectorAll('#first-row .oneletter')
+    let currentLoop = 0
+    // letterRow[0].innerText = '1'
+    console.log('test' + letterRow[0])
+    // letterRow[0].innerText = letter;
+
+    for (oneLetter of letterRow) {
+               // oneLetter[currentLoop].innerHTML = letter;
+        if (letter[currentLoop]) {
+            letterRow[currentLoop].innerHTML = letter[currentLoop]
+        }
+        else if (!(letter[currentLoop])) {
+            letterRow[currentLoop].innerHTML = ''
+        }
+        // console.log('oneletter' + oneLetter)
+        // console.log(oneLetter[0])
+        currentLoop++
+    }
+
+    // for (oneLetter of letterRow) {
+    //     // oneLetter[currentLoop].innerHTML = letter;
+    //     letterRow[currentLoop].innerHTML = letter
+    //     console.log('oneletter' + oneLetter)
+    //     // console.log(oneLetter[0])
+    //     currentLoop++
+    // }
+}
+
+// updateDivs('a');
+
+// console.log(userInput)
 
 //Make a random number whenever this function is run
 
@@ -15,13 +103,13 @@ function randomNumList(length) {
 let letterDivs = document.querySelectorAll('.letters');
 // let divs = document.getElementsByClassName('letter')
 
-console.log(letterDivs)
+// console.log(letterDivs)
 // console.log(divs)
 
 
-for (div of letterDivs) {
-    console.log('test' + div.classList)
-}
+// for (div of letterDivs) {
+//     console.log('test' + div.classList)
+// }
 
 
 // New variable for the random num based on length of words array
@@ -43,7 +131,16 @@ let newH = document.createElement('h2');
 newH.innerHTML= randomWord;
 output.appendChild(newH);
 
+//push todays random wordle to an array
 
+function todaysWordle() {
+
+    let wordleArr = randomWord.split('')
+    console.log(wordleArr)
+    return wordleArr
+}
+
+todaysWordle()
 // const input = prompt('Where are your green letters?')
 
 
